2fc03f1814314ce2e233d421177b6e32f4735719
Richard Gross <richard.gross@maibornwolff.de>
2022-08-20 12:04:15 +0200

    R extract types and opts for the handle into separate objects

--- src/user-routes.ts
+++ src/user-routes.ts
@@ -18,36 +18,34 @@ export async function userRoutes(fastify: FastifyInstance) {
 
     console.info('Registering user routes');
 
-    fastify.get<{ Querystring: UserQueryDto, Reply: UserCollectionDto }>(
-        '/users',
-        {
-            schema: {
-                querystring: Type.Ref(UserQuerySchema),
-                response: {
-                    200: Type.Ref(UserCollectionSchema),
-                    400: Type.Ref(ValidationFailedSchema),
-                },
+    type QueryUsersTypes = { Querystring: UserQueryDto, Reply: UserCollectionDto };
+    const queryUsersOpts = {
+        schema: {
+            querystring: Type.Ref(UserQuerySchema),
+            response: {
+                200: Type.Ref(UserCollectionSchema),
+                400: Type.Ref(ValidationFailedSchema),
             },
         },
-        (request, reply) => {
+    };
+    fastify.get<QueryUsersTypes>('/users', queryUsersOpts, (request, reply) => {
             // The `name` and `mail` types are automatically inferred
             const {name, mail} = request.query;
             reply.status(200).send([{name: name ?? 'Alex', mail: mail ?? 'non@example.org'}]);
         }
     );
 
-    fastify.post<{ Body: UserDto, Reply: UserDto }>(
-        '/users',
-        {
-            schema: {
-                body: Type.Ref(UserSchema),
-                response: {
-                    200: Type.Ref(UserSchema),
-                    400: Type.Ref(ValidationFailedSchema),
-                },
+    type CreateUserTypes = { Body: UserDto, Reply: UserDto };
+    const createUserOpts = {
+        schema: {
+            body: Type.Ref(UserSchema),
+            response: {
+                200: Type.Ref(UserSchema),
+                400: Type.Ref(ValidationFailedSchema),
             },
         },
-        (request, reply) => {
+    };
+    fastify.post<CreateUserTypes>('/users', createUserOpts,(request, reply) => {
             // The `name` and `mail` types are automatically inferred
             const {name, mail} = request.body;
             reply.status(200).send({name, mail});
